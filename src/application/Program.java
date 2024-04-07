d229a98dbf3c80291078bc9d584ee2f009c4b427
Welison Joras <welison.joras@hotmail.com>
2024-04-07 20:14:15 -0300

    created department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import mode.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "books");
+		
+		System.out.println(obj);
+
+	}
+
+}
