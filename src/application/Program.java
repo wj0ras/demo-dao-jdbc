1c858a2c26c73422b65c8567c600511be61ab73e
Welison Joras <welison.joras@hotmail.com>
2024-04-07 20:25:30 -0300

    created seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import mode.entities.Department;
+import mode.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "books");
+		Seller seller = new Seller(21, "Welison", "wjoras1@gmail.com", new Date(), 1300.0, obj);
 		
-		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
