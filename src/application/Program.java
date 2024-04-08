70fe1521171dcfdd4a9d2cc05c30dee52ba89c7a
Welison Joras <welison.joras@hotmail.com>
2024-04-07 21:55:30 -0300

    reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,17 @@
 package application;
 
-import java.util.Date;
-
-import mode.entities.Department;
+import mode.dao.DaoFactory;
+import mode.dao.SellerDao;
 import mode.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "books");
-		Seller seller = new Seller(21, "Welison", "wjoras1@gmail.com", new Date(), 1300.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(2);
 		
 		System.out.println(seller);
-
 	}
 
 }
--- src/mode/dao/DaoFactory.java
+++ src/mode/dao/DaoFactory.java
@@ -1,5 +1,12 @@
 package mode.dao;
 
+import db.DB;
+import mode.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
 
 }
